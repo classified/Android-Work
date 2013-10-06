74b5619665ba05ebcd13434a514cb67b70ec82af
Rajan Palaniya <razorpaul@gmail.com>
2013-10-06 21:52:59 +0530

    DataBase Demo

---
+++ Database/src/com/android/my/database/DBHelper.java
@@ -0,0 +1,38 @@
+package com.android.my.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+	static final String DATABASE = "Datax.db";
+	static final int VERSION = 1;
+	static final String TABLE = "emp";
+	static final String C_ID = "_id";
+	static final String C_NAME = "ename";
+	static final String C_DESIGNATION = "designation";
+	static final String C_SALARY = "salary";
+
+	public DBHelper(Context context) {
+		super(context, DATABASE, null, VERSION);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL("CREATE TABLE " + TABLE + " ( " + C_ID
+				+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + C_NAME + " text, "
+				+ C_DESIGNATION + " TEXT, " + C_SALARY + " text) ");
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		db.execSQL("Drop table " + TABLE);
+		onCreate(db);
+	}
+
+}
---
+++ Database/src/com/android/my/database/MainActivity.java
@@ -0,0 +1,89 @@
+package com.android.my.database;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	EditText txtName, txtDesig, txtSalary;
+	Button btnAdd;
+	SimpleCursorAdapter adapter;
+	DBHelper helper;
+	SQLiteDatabase db;
+	ListView disp;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		helper = new DBHelper(this);
+		txtName = (EditText) findViewById(R.id.txtName);
+		txtDesig = (EditText) findViewById(R.id.txtGraduation);
+		txtSalary = (EditText) findViewById(R.id.txtSalary);
+		btnAdd = (Button) findViewById(R.id.button1);
+		disp = (ListView) findViewById(R.id.dispID);
+		fetchdata();
+		btnAdd.setOnClickListener(this);
+	}
+
+	@SuppressWarnings("deprecation")
+	private void fetchdata() {
+		// TODO Auto-generated method stub
+		db = helper.getReadableDatabase();
+		Cursor c = db.query(DBHelper.TABLE, null, null, null, null, null, null);
+		adapter = new SimpleCursorAdapter(MainActivity.this, R.layout.row, c,
+				new String[] { DBHelper.C_NAME, DBHelper.C_SALARY,
+						DBHelper.C_DESIGNATION }, new int[] { R.id.lblName,
+						R.id.lblSalary, R.id.lblDesig });
+		disp.setAdapter(adapter);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		if (arg0 == btnAdd) {
+			ContentValues value = new ContentValues();
+			value.put(DBHelper.C_NAME, txtName.getText().toString());
+			value.put(DBHelper.C_DESIGNATION, txtDesig.getText().toString());
+			value.put(DBHelper.C_SALARY, txtSalary.getText().toString());
+			db = helper.getWritableDatabase();
+			db.insert(DBHelper.TABLE, null, value);
+			db.close();
+			clearField();
+			Toast.makeText(MainActivity.this, "RECORD UPDATED SUCCESSFULLY",
+					Toast.LENGTH_LONG).show();
+
+			fetchdata();
+
+		}
+
+	}
+
+	private void clearField() {
+		// TODO Auto-generated method stub
+		txtName.setText("");
+		txtDesig.setText("");
+		txtSalary.setText("");
+
+	}
+
+}
